7695125cc8c9fc25aab27ac41e0c3ea70b0a8fed
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-05-21 23:18:52 +0600

    banner create

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import image from '../../Images/all parts pic.jpg'
+import MainButton from '../Shared/MainButton';
+
+const Banner = () => {
+    return (
+        <div>
+            <div class="hero min-h-screen">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src= {image} 
+    class="max-w-sm rounded-lg shadow-2xl pic" alt='' />
+    <div className='text-area'>
+      <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
+      <p class="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quaerat laudantium quisquam. Iure corporis tempora excepturi numquam vero doloremque ut.</p>
+      <MainButton>Get Started</MainButton>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
