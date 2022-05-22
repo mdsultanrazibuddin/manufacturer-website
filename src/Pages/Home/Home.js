8bcc28d03a2e6d92a7708d8554e9bbc03357901d
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-05-23 00:53:30 +0600

    summary prepared

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
     return (
         <div>
             <Banner/>
+            <h3 className='text-5xl font-bold text-secondary text-center'>Top Parts</h3>  
             <Parts/>
             <Summary/>
             <AboutUs/>
--- src/Pages/Home/Summary.js
+++ src/Pages/Home/Summary.js
@@ -1,38 +1,58 @@
 import React from 'react';
+import { BsFlag} from 'react-icons/bs';
+import {VscFeedback} from 'react-icons/vsc';
+import { BsFillPeopleFill} from 'react-icons/bs';
+import { AiOutlineFundProjectionScreen} from 'react-icons/ai';
 
 
 const Summary = () => {
     return (
         <div className='text-center '>
-            <h1>Millions Business Trust Us</h1>
+            <h1 className='text-5xl font-bold text-secondary'>Millions Business Trust Us</h1>
+            <p className='text-2xl font-bold my-3'>TRY TO UNDERSTAND USER EXPACTATION</p>
 
-            <div class="stats stats-vertical lg:stats-horizontal shadow text-2xl text-center bg-info">
+            <div class="stats stats-vertical lg:stats-horizontal shadow text-2xl text-center my-5 ">
   
   <div class="stat">
-  <div class="stat-figure text-primary">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-2xl" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
-</svg>
+    <div className='p-5 text-primary'>
+      <BsFlag/>
     </div>
-    <div class="stat-title">Countries</div>
+    
     <div class="stat-value">85</div>
+    <div class="stat-title text-base text-primary">Countries</div>
+    
     
   </div>
   
   <div class="stat">
-    <div class="stat-title">Complete Projects</div>
+    <div className='p-5 text-primary'>
+    <AiOutlineFundProjectionScreen/>
+    </div>
+    
     <div class="stat-value">590+</div>
+    <div class="stat-title text-base text-primary">Complete Projects</div>
+    
     
   </div>
   
   <div class="stat">
-    <div class="stat-title">Happy Clients</div>
+  <div className='p-5 text-primary'>
+  <BsFillPeopleFill/>
+  </div>
+    
     <div class="stat-value">273+</div>
+    <div class="stat-title text-base text-primary">Happy Clients</div>
+    
     
   </div>
   <div class="stat">
-    <div class="stat-title">Feedbacks</div>
+  <div className='p-5 text-primary'>
+  <VscFeedback/>
+  </div>
+    
     <div class="stat-value">432+</div>
+    <div class="stat-title text-base text-primary">Feedbacks</div>
+    
     
   </div>
   
